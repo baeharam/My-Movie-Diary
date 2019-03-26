841afb516f390a71f26ea129992ce6cfc69125ed
bae haram <gkfkagkfka12@naver.com>
2019-03-26 18:50:54 +0900

    Fix height
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -98,6 +98,7 @@ class _IntroScreenState extends State<IntroScreen> with TickerProviderStateMixin
           return SingleChildScrollView(
             child: Container(
               width: double.infinity,
+              height: MediaQuery.of(context).size.height,
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage(backgroundImageList[_backgroundImageAnimation.value]),
