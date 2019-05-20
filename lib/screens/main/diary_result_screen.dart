38ff8792449da9380b1119bb7abc48c71ffc6f78
bae haram <gkfkagkfka12@naver.com>
2019-05-20 11:59:48 +0900

    Divide activity per state
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

--- lib/screens/main/diary_result_screen.dart
+++ lib/screens/main/diary_result_screen.dart
@@ -1,10 +1,16 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:mymovie/logics/diary_edit/diary_edit.dart';
+import 'package:mymovie/logics/diary_result/diary_result.dart';
 import 'package:mymovie/models/diary_model.dart';
+import 'package:mymovie/resources/colors.dart';
 import 'package:mymovie/screens/main/diary_edit_screen.dart';
 import 'package:mymovie/screens/sub/diary_result_body.dart';
+import 'package:mymovie/utils/bloc_navigator.dart';
+import 'package:mymovie/utils/bloc_snackbar.dart';
 import 'package:mymovie/utils/service_locator.dart';
 
 class DiaryResultScreen extends StatefulWidget {
@@ -20,18 +26,21 @@ class DiaryResultScreen extends StatefulWidget {
 class _DiaryResultScreenState extends State<DiaryResultScreen> {
 
   final DiaryEditBloc _diaryEditBloc = sl.get<DiaryEditBloc>();
+  final DiaryResultBloc _diaryResultbloc = sl.get<DiaryResultBloc>();
   int _randomIndex;
 
   @override
   void initState() {
     super.initState();
     _diaryEditBloc.dispatch(DiaryEditEventStateClear());
+    _diaryResultbloc.dispatch(DiaryResultEventStateClear());
     _randomIndex = Random().nextInt(widget.diaryModel.movieStillCutList.length);
   }
 
   @override
   void dispose() {
     _diaryEditBloc.dispatch(DiaryEditEventStateClear());
+    _diaryResultbloc.dispatch(DiaryResultEventStateClear());
     super.dispose();
   }
 
@@ -60,9 +69,30 @@ class _DiaryResultScreenState extends State<DiaryResultScreen> {
           )
         ],
       ),
-      body: DiaryResultBody(
-        diaryModel: widget.diaryModel,
-        randomIndex: _randomIndex
+      body: BlocBuilder<DiaryResultEvent, DiaryResultState>(
+        bloc: _diaryResultbloc,
+        builder: (context, state){
+          if(state.isDeleteLoading) {
+            return Container(
+              alignment: Alignment.center,
+              color: AppColor.background,
+              child: SpinKitWave(
+                color: Colors.white,
+                size: 50.0,
+              )
+            );
+          }
+          if(state.isDeleteFailed){
+            BlocSnackbar.show(context, '삭제에 실패했습니다.');
+          }
+          if(state.isDeleteSucceeded){
+            BlocNavigator.pop(context);
+          }
+          return DiaryResultBody(
+            diaryModel: widget.diaryModel,
+            randomIndex: _randomIndex
+          );
+        }
       ),
     );
   }
